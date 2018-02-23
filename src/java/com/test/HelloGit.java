3ec2b6860324308869d553260788ed392132d88b
hbisme <343122422@qq.com>
2018-02-23 21:52:13 +0800

    Default1

--- src/java/com/test/HelloGit.java
+++ src/java/com/test/HelloGit.java
@@ -2,10 +2,10 @@ package com.test;
 
 public class HelloGit {
     public static void main(String[] args) {
-        System.out.println("hello git " +
-                "hello hb!");
+//        System.out.println("hello git " +
+//                "hello hb!");
         System.out.println("hello hb");
-        
+
 
     }
 }

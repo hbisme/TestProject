a56a7fec624de66685cda6d64e885683b7b6c60d
hbisme <343122422@qq.com>
2018-02-23 21:43:55 +0800

    first3

--- src/java/com/test/HelloGit.java
+++ src/java/com/test/HelloGit.java
@@ -4,6 +4,7 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("hello git " +
                 "hello hb!");
+        System.out.println("hello hb");
         
 
     }
